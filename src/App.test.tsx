679a695bedb94a6f46124207fca7f4d74946baad
Natalia-Havashi <natali.gavashi@gmail.com>
2023-12-04 15:51:15 +0200

    o

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
+
+Створіть загальну функцію getProperty яка приймає обєкт та ключ як рядок
+Функція повинна повертати значення цього ключа з обєкта.
